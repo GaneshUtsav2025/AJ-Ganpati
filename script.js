18d15250d7dfce50062480c07bc40e74da614e32
Ganesh Utsav <dean.kunal2025@gmail.com>
2025-08-25 14:16:51 +0530

    Working Copy

--- script.js
+++ script.js
@@ -1,28 +1,320 @@
 const ganpatis = [
   {
-    name: "Bal Ganesh",
-    price: "₹850",
-    size: "12 inches",
-    image: "images/gpGanpati_num_1_Type_1_Name_balGaneshGanpati_code_0001.jpeg",
-    Code: "01"
+    "name": "Child Ganesha",
+    "price": "₹201",
+    "size": "6 inches",
+    "image": "images/1.jpeg",
+    "Code": "01"
   },
   {
-    name: "Eco Friendly Ganpati",
-    price: "₹1200",
-    size: "18 inches",
-    image: "images/gpGanpati_num_2_Type_2_Name_mayurMukutGanpati_code_0002.jpeg",
-    Code: "02"
+    "name": "Child Ganesha",
+    "price": "₹201",
+    "size": "6 inches",
+    "image": "images/2.jpeg",
+    "Code": "02"
   },
   {
-    name: "Friendly Ganpati",
-    price: "₹1100",
-    size: "18 inches",
-    image: "images/gpGanpati_num_3_Type_2_Name_shwetMarbleGanpati_code_0003.jpeg",
-    Code: "03"
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/3.jpeg",
+    "Code": "03"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/4.jpeg",
+    "Code": "04"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/5.jpeg",
+    "Code": "05"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹201",
+    "size": "6 inches",
+    "image": "images/6.jpeg",
+    "Code": "06"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/7.jpeg",
+    "Code": "07"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/8.jpeg",
+    "Code": "08"
+  },
+  {
+    "name": "Bal Chaturbhuj",
+    "price": "₹301",
+    "size": "12 inches",
+    "image": "images/9.jpeg",
+    "Code": "09"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/10.jpeg",
+    "Code": "10"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹251",
+    "size": "8 inches",
+    "image": "images/11.jpeg",
+    "Code": "11"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹301",
+    "size": "9 inches",
+    "image": "images/12.jpeg",
+    "Code": "12"
+  },
+  {
+    "name": "Child Ganesha",
+    "price": "₹301",
+    "size": "9 inches",
+    "image": "images/13.jpeg",
+    "Code": "13"
+  },
+  {
+    "name": "Sinhasanadhishwar",
+    "price": "₹401",
+    "size": "10 inches",
+    "image": "images/14.jpeg",
+    "Code": "14"
+  },
+  {
+    "name": "Maha Ganpati",
+    "price": "₹501",
+    "size": "13 inches",
+    "image": "images/15.jpeg",
+    "Code": "15"
+  },
+  {
+    "name": "Balganesh",
+    "price": "₹901",
+    "size": "13–14 inches",
+    "image": "images/18.jpeg",
+    "Code": "18"
+  },
+  {
+    "name": "Sidhivinayak Ganpati",
+    "price": "₹901",
+    "size": "13–14 inches",
+    "image": "images/20.jpeg",
+    "Code": "20"
+  },
+  {
+    "name": "Sidhivinayaka",
+    "price": "₹901",
+    "size": "12 inches",
+    "image": "images/21.jpeg",
+    "Code": "21"
+  },
+  {
+    "name": "Chaturbhuj Ganpati",
+    "price": "₹1001",
+    "size": "15–16 inches",
+    "image": "images/22.jpeg",
+    "Code": "22"
+  },
+  {
+    "name": "Sidhivinayak Ganpati",
+    "price": "₹1001",
+    "size": "15–16 inches",
+    "image": "images/24.jpeg",
+    "Code": "24"
+  },
+  {
+    "name": "Sinhasanadhishwar",
+    "price": "₹1001",
+    "size": "15–16 inches",
+    "image": "images/26.jpeg",
+    "Code": "26"
+  },
+  {
+    "name": "Moshakraj",
+    "price": "₹1151",
+    "size": "17–19 inches",
+    "image": "images/28.jpeg",
+    "Code": "28"
+  },
+  {
+    "name": "Sinhasanadhishwar",
+    "price": "₹1151",
+    "size": "17–19 inches",
+    "image": "images/29.jpeg",
+    "Code": "29"
+  },
+  {
+    "name": "Chturbhuj Ganpati",
+    "price": "₹1151",
+    "size": "17–19 inches",
+    "image": "images/32.jpeg",
+    "Code": "32"
+  },
+  {
+    "name": "Chaturbhuj Ganpati",
+    "price": "₹1251",
+    "size": "18–20 inches",
+    "image": "images/35.jpeg",
+    "Code": "35"
+  },
+  {
+    "name": "Balganesh Krishna",
+    "price": "₹1251",
+    "size": "18–20 inches",
+    "image": "images/36.jpeg",
+    "Code": "36"
+  },
+  {
+    "name": "Chaturbhuj Sidhivinayka",
+    "price": "₹1251",
+    "size": "18–20 inches",
+    "image": "images/37.jpeg",
+    "Code": "37"
+  },
+  {
+    "name": "Dagdushet",
+    "price": "₹1501",
+    "size": "20–22 inches",
+    "image": "images/38.jpeg",
+    "Code": "38"
+  },
+  {
+    "name": "Sidhivinayaka",
+    "price": "₹1251",
+    "size": "18–20 inches",
+    "image": "images/39.jpeg",
+    "Code": "39"
+  },
+  {
+    "name": "Chaturbhuj Ganpati",
+    "price": "₹1501",
+    "size": "20–25 inches",
+    "image": "images/40.jpeg",
+    "Code": "40"
+  },
+  {
+    "name": "Tirumala Ganesha",
+    "price": "₹2101",
+    "size": "24–25 inches",
+    "image": "images/41.jpeg",
+    "Code": "41"
+  },
+  {
+    "name": "Vamamukhi Ganpati",
+    "price": "₹1501",
+    "size": "20–25 inches",
+    "image": "images/45.jpeg",
+    "Code": "45"
+  },
+  {
+    "name": "Sidhivinayak Ganpati",
+    "price": "₹2501",
+    "size": "27–30 inches",
+    "image": "images/48.jpeg",
+    "Code": "48"
+  },
+  {
+    "name": "Chaturbhuj Ganpati",
+    "price": "₹2501",
+    "size": "27–30 inches",
+    "image": "images/49.jpeg",
+    "Code": "49"
+  },
+  {
+    "name": "Bal Ganesh",
+    "price": "₹3101",
+    "size": "27–30 inches",
+    "image": "images/52.jpeg",
+    "Code": "52"
+  },
+  {
+    "name": "Sidhivinayaka",
+    "price": "₹3101",
+    "size": "27–30 inches",
+    "image": "images/53.jpeg",
+    "Code": "53"
+  },
+  {
+    "name": "Vijaya Ganpati",
+    "price": "₹3501",
+    "size": "27–29 inches",
+    "image": "images/54.jpeg",
+    "Code": "54"
+  },
+  {
+    "name": "Vignaharta Raja",
+    "price": "₹3501",
+    "size": "28–30 inches",
+    "image": "images/56.jpeg",
+    "Code": "56"
+  },
+  {
+    "name": "Shatbhuja Ganpati",
+    "price": "₹4001",
+    "size": "32–34 inches",
+    "image": "images/57.jpeg",
+    "Code": "57"
+  },
+  {
+    "name": "Swami Samarth",
+    "price": "₹3501",
+    "size": "28–30 inches",
+    "image": "images/59.jpeg",
+    "Code": "59"
+  },
+  {
+    "name": "Vasudev Maharaj",
+    "price": "₹3501",
+    "size": "28–30 inches",
+    "image": "images/60.jpeg",
+    "Code": "60"
+  },
+  {
+    "name": "Lalbagcha Raja",
+    "price": "₹4501",
+    "size": "32–34 inches",
+    "image": "images/62.jpeg",
+    "Code": "62"
+  },
+  {
+    "name": "Sinhasanadhishwar",
+    "price": "₹4501",
+    "size": "34 inches",
+    "image": "images/63.jpeg",
+    "Code": "63"
+  },
+  {
+    "name": "Mhalar",
+    "price": "₹4501",
+    "size": "32–34 inches",
+    "image": "images/66.jpeg",
+    "Code": "66"
+  },
+  {
+    "name": "Dagdushet",
+    "price": "₹4501",
+    "size": "34 inches",
+    "image": "images/68.jpeg",
+    "Code": "68"
   }
-  // Continue adding entries for all 65 Ganpatis
 ];
-
 const gallery = document.getElementById('gallery');
 const sortSelect = document.getElementById('sortSelect');
 
